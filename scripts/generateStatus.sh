db730782526018ce1bc033ec85b73e3f7ddaaaa6
Shota Izumiyama <shota@izumiyama.net>
2021-04-25 14:02:01 +0900

    Minor modification about display style  Changes to be committed: 	modified:   scripts/generateStatus.sh

--- scripts/generateStatus.sh
+++ scripts/generateStatus.sh
@@ -16,16 +16,16 @@ indNext=$(echo $header | awk '{print index($0, "NEXT")}')
 indLast=$(echo $header | awk '{print index($0, "LAST")}')
 indTimer=$(echo $header | awk '{print index($0, "UNIT")}')
 indName=$(echo $header | awk '{print index($0, "ACTIVATES")}')
-for line in $(systemctl --user list-timers | head -n-3 | tail -n+2);
+for line in $(systemctl --user list-timers | head -n-3 | tail -n+2 | grep record);
 do
     put "<tr>"
-    put "$(echo $line | awk '{print "<td>"substr($0,'$indNext',23)"("substr($0,'$indNext+28','$indLast-$indNext-29')")</td><td>"substr($0,'$indLast',23)"("substr($0,'$indLast+28','$indTimer-$indLast-29')")</td><td>"substr($0,'$indName',99)"</td>"}')"
+    put "$(echo $line | awk '{print "<td>"substr($0,'$indNext',23)" ("substr($0,'$indNext+28','$indLast-$indNext-29')")</td><td>"substr($0,'$indLast',23)" ("substr($0,'$indLast+28','$indTimer-$indLast-29')")</td><td>"substr($0,'$indName',99)"</td>"}')"
     local laststatus=$(echo $line | awk '{print substr($0,'$indName',50)}' | xargs systemctl --user status | grep "Main PID")
     #echo $laststatus
     if [ -z "$laststatus" ]; then
         put "<td>N/A</td>"
     else
-        put "$(echo $laststatus| awk 'BEGIN{FS="[(,=)]"}{print "<td>"$3"("$5")</td>"}')"
+        put "$(echo $laststatus| awk 'BEGIN{FS="[(,=)]"}{print "<td>"$3" ("$5")</td>"}')"
     fi
     put "</tr>"
 done
